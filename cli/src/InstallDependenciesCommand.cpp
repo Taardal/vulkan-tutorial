b26c37fcde8c72298ccd9b092382af598ff25b46
Torbjørn Årdal <torbjorn.aardal@gmail.com>
2022-02-23 11:33:04 +0100

    foo

--- cli/src/InstallDependenciesCommand.cpp
+++ cli/src/InstallDependenciesCommand.cpp
@@ -1,11 +1,33 @@
 #include "InstallDependenciesCommand.h"
 #include <iostream>
 #include <sstream>
+#include <vector>
+
+namespace VulkandemoCLI
+{
+    std::string InstallDependenciesCommand::Dependency::ToString()
+    {
+        std::stringstream ss;
+        ss << Name << "=" << Version;
+        return ss.str();
+    }
+
+    static InstallDependenciesCommand::Dependency FromString(const std::string& string)
+    {
+        int equalSignPosition = string.find("=");
+        InstallDependenciesCommand::Dependency dependency;
+        dependency.Name = string.substr(0, equalSignPosition);
+        dependency.Version = string.substr(equalSignPosition + 1, string.length());
+        return dependency;
+    }
+}
 
 namespace VulkandemoCLI
 {
     const char* InstallDependenciesCommand::NAME = "deps";
-    const char* InstallDependenciesCommand::LIB_FOLDER = "lib";
+    const char* InstallDependenciesCommand::DESCRIPTION = "Install dependencies (git submodules)";
+    const char* InstallDependenciesCommand::DEPENDENCIES_DIRECTORY_NAME = "lib";
+    const char* InstallDependenciesCommand::DEPENDENCIES_FILENAME = ".dependencies";
 
     const char* InstallDependenciesCommand::GetName() const
     {
@@ -14,37 +36,70 @@ namespace VulkandemoCLI
 
     const char* InstallDependenciesCommand::GetDescription() const
     {
-        return "Install dependencies (git submodules)";
+        return DESCRIPTION;
     }
 
     void InstallDependenciesCommand::Execute() const
     {
-        printf("%s\n", "NOTICE ME");
-        printf("%s\n", getenv("GLFW"));
-        printf("%s\n", getenv("GLM"));
-        printf("%s\n", getenv("SPDLOG"));
-        printf("\n");
-
         printf("Installing git submodules...\n");
-        InstallGitSubmodules();
+
+        //printf("git submodule update --init\n");
+        std::system("git submodule update --init");
+
         printf("Setting git submodule versions...\n");
-        SetGitSubmoduleVersion("glfw", getenv("GLFW"));
-        SetGitSubmoduleVersion("glm", getenv("GLM"));
-        SetGitSubmoduleVersion("spdlog", getenv("SPDLOG"));
+        const std::vector<Dependency>& dependencies = ReadDependencies();
+        for (Dependency dependency : dependencies)
+        {
+            std::stringstream ss;
+            ss << "cd " << DEPENDENCIES_DIRECTORY << "/" << dependency.Name << " && git checkout " << dependency.Version;
+            std::string command = ss.str();
+            std::system(command.c_str());
+        }
+        /*
+        FileSystem fileSystem;
+        std::vector<Env> envs;
+        std::istringstream iss(fileSystem.ReadFile("lib/.versions"));
+        for (std::string line; std::getline(iss, line);)
+        {
+            Env env = Env::FromString(line);
+            printf("-- Using [%s] version [%s]\n", env.Key.c_str(), env.Value.c_str());
+            std::stringstream ss;
+            ss << "cd " << DEPENDENCIES_DIRECTORY << "/" << env.Key.c_str() << " && git checkout " << env.Value.c_str();
+            std::string command = ss.str();
+            std::system(command.c_str());
+        }
+        */
+    }
+
+    std::vector<InstallDependenciesCommand::Dependency> InstallDependenciesCommand::ReadDependencies() const
+    {
+        std::vector<Dependency> dependencies;
+        FileSystem fileSystem;
+        std::istringstream iss(fileSystem.ReadFile(DEPENDENCIES_FILENAME));
+        for (std::string line; std::getline(iss, line);)
+        {
+            //dependencies.push_back(Dependency::FromString(line));
+            int equalSignPosition = line.find("=");
+            Dependency dependency;
+            dependency.Name = line.substr(0, equalSignPosition);
+            dependency.Version = line.substr(equalSignPosition + 1, line.length());
+            dependencies.push_back(dependency);
+        }
+        return dependencies;
     }
 
     void InstallDependenciesCommand::InstallGitSubmodules() const
     {
         printf("git submodule update --init\n");
-        //std::system("git submodule update --init");
+        std::system("git submodule update --init");
     }
 
-    void InstallDependenciesCommand::SetGitSubmoduleVersion(const char* folder, const char* version) const
+    void InstallDependenciesCommand::SetGitSubmoduleVersion(const char* directory, const char* version) const
     {
-        printf("-- Using [%s] version [%s]\n", folder, version);
+        printf("-- Using [%s] version [%s]\n", directory, version);
         std::stringstream ss;
-        ss << "cd " << LIB_FOLDER << "/" << folder << " && git checkout " << version;
+        ss << "cd " << DEPENDENCIES_DIRECTORY << "/" << directory << " && git checkout " << version;
         std::string command = ss.str();
-        //std::system(command.c_str());
+        std::system(command.c_str());
     }
 }
--- cli/src/InstallDependenciesCommand.h
+++ cli/src/InstallDependenciesCommand.h
@@ -1,11 +1,25 @@
 #pragma once
 
 #include "Command.h"
+#include "FileSystem.h"
+#include <string>
+#include <sstream>
 
 namespace VulkandemoCLI
 {
     class InstallDependenciesCommand : public Command
     {
+    public:
+        struct Dependency
+        {
+            std::string Name;
+            std::string Version;
+
+            std::string ToString();
+
+            static Dependency FromString(const std::string& string);
+        };
+
     public:
         const char* GetName() const override;
 
@@ -18,10 +32,39 @@ namespace VulkandemoCLI
 
         void SetGitSubmoduleVersion(const char* folder, const char* version) const;
 
+        std::vector<Dependency> ReadDependencies() const;
+
     public:
         static const char* NAME;
+        static const char* DESCRIPTION;
 
     private:
-        static const char* LIB_FOLDER;
+        static const char* DEPENDENCIES_DIRECTORY_NAME;
+        static const char* DEPENDENCIES_FILENAME;
+    };
+}
+
+namespace VulkandemoCLI
+{
+    struct Env
+    {
+        std::string Key;
+        std::string Value;
+
+        std::string ToString()
+        {
+            std::stringstream ss;
+            ss << Key << "=" << Value;
+            return ss.str();
+        }
+
+        static Env FromString(const std::string& envString)
+        {
+            int equalSignPosition = envString.find("=");
+            Env env;
+            env.Key = envString.substr(0, equalSignPosition);
+            env.Value = envString.substr(equalSignPosition + 1, envString.length());
+            return env;
+        }
     };
 }
