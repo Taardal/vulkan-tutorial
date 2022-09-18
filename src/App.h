6ea33e89dcfbfcd4e9d5b71a5d011075a1ac0b64
Torbjørn Årdal <torbjorn.aardal@gmail.com>
2022-09-18 10:44:09 +0200

    cleanup

--- src/App.h
+++ src/App.h
@@ -74,16 +74,11 @@ namespace Vulkandemo {
         VulkanImage* vulkanDepthImage;
         VkImageView depthImageView = VK_NULL_HANDLE;
 
-        const uint32_t WIDTH = 800;
-        const uint32_t HEIGHT = 600;
-
         const std::string MODEL_PATH = "models/viking_room.obj";
         const std::string TEXTURE_PATH = "textures/viking_room.png";
 
         std::vector<Vertex> vertices;
         std::vector<uint32_t> indices;
-        //VkBuffer vertexBuffer;
-        //VkDeviceMemory vertexBufferMemory;
 
     public:
         explicit App(Config config);
